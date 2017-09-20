521c171b8a537d45342c16c17e6d69a823a65eb2
Hoehlinger, Sébastien <shoehlinger@reseau-gds.fr>
2017-09-20 17:28:33 +0200

    Add project files.

---
+++ MVVMApp1/ViewModels/BaseViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMApp1.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+    }
+
+    }
+
+    public class MainViewModel : BaseViewModel
+    {
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _firstname;
+
+        public string FirsName
+        {
+            get { return _firstname; }
+            set
+            {
+                _firstname = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+    }
+
+
+
+}
